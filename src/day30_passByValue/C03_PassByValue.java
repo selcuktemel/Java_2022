d7a39907d0f9a123dce5b683a8b4f48b796baf43
SelcukTemel <selcuktemell2@gmail.com>
2022-03-29 22:22:58 +0300

    29.03.2022

--- src/day30_passByValue/C03_PassByValue.java
+++ src/day30_passByValue/C03_PassByValue.java
@@ -2,14 +2,28 @@ package day30_passByValue;
 
 public class C03_PassByValue {
     public static void main(String[] args) {
+
+        /*
+        Java Pass By Value bir programlama dilidir.
+        Yani ; bir primitive variable'i argument olarak methoda yollarsaniz Java o variable'i degil degerini(value ) method'a aktarir (pass)
+         */
+
         double etiketFiyati=100;
-        yuzdeOnIndirimYap(etiketFiyati);
-        yuzdeOnIndirimYap(etiketFiyati);
+        yuzdeOnIndirimYap(etiketFiyati);//metohodu birincikez yazdirdi
+        yuzdeOnIndirimYap(etiketFiyati);// methodu ikinci kez yazdirdi. ayni yeyi yazdirdi atama yapmamaıza ragmen
+        System.out.println("iki indirimden sonra main method'da etiket fiyati : "+etiketFiyati); //etiket fiyati degismedi.
+
     }
 
-    public static void yuzdeOnIndirimYap(double etiketFiyati) {
-
+    public static void yuzdeOnIndirimYap(double etiketFiyati) {// burada etiket fiyati diye olusturgum sey yukarida olusturdugum variabledan daha farkli bir sey;
+                                                               // eger class levelde variable olusturursak
         etiketFiyati=etiketFiyati*0.90;
         System.out.println("sizin icin %10 indrimli fiyatimiz : "+ etiketFiyati);
+
+        /*
+        pass by value: ben orjinal etiketFiyatını yazıp methoda yolladım oraya gittiginde de adi etiket fiyati ama omethoda götürdügü value deger
+        etiket fiyatını variable olarak değil deger(value) olarak gidiyor. methoddaki variablenin isminin degişmesi herhangi bir seyi degistirmez.
+        kısaca methoda gönderdiğimiz degerdir. main methoddaki variablenin degeri degismez.
+         */
     }
 }
---
+++ src/day30_passByValue/C05_PassByValue.java
@@ -0,0 +1,20 @@
+package day30_passByValue;
+
+public class C05_PassByValue {
+    //kalici indirim icin ikinci yöntem
+
+    public static void main(String[] args) {
+
+        double etiketFiyati = 100; // local varible
+        double indirimYuzdesi = 10;// local variable
+       etiketFiyati= kaliciIndirimYap(etiketFiyati, indirimYuzdesi);
+        System.out.println("main method'da indirim sonrasi etiket fiyati : "+ etiketFiyati);
+    }
+
+    private static double kaliciIndirimYap(double etiketFiyati, double indirimYuzdesi) {
+
+            etiketFiyati = etiketFiyati * (100 - indirimYuzdesi) / 100;
+            System.out.println("indirimli fiyat : " + etiketFiyati);
+            return etiketFiyati;
+    }
+}
