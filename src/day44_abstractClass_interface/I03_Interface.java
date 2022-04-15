004cf40e18eb8f5742703c36d73dd2e1fe8b4bf2
SelcukTemel <selcuktemell2@gmail.com>
2022-04-15 22:05:05 +0300

    15.04.2022

---
+++ src/day44_abstractClass_interface/I03_Interface.java
@@ -0,0 +1,14 @@
+package day44_abstractClass_interface;
+
+public interface I03_Interface {
+
+    /*
+    tam soyutlama /full abstraction olusturmak istesen asbstact class'lari değil inderface kullaniriz.
+    interfalcelerde sadece abstract methodlari bulunur.
+    neleri yapmamin gerektiğini söyleyen  to do list  ==yapılacakllar listesidir.
+     */
+
+    public static final int SAYI=20;
+    int SAYİ2=20;
+
+}
